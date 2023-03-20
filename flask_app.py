e3c474a2dd04631b383e8b3ce368a875f39c6e98
Jani M <jani.maki@seamk.fi>
2023-03-20 10:26:52 +0200

    post lisätty

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -8,12 +9,18 @@ lampotilat = [
     {'x':3, 'y':18}
 ]
 
-paivat = ['Maanantai', 'Tiistai', 'Keskiviikko']
+paivat = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai']
 
 # kirjoita selaimeen localhost:5000/api, että voit tarkastella sivua
 @app.route('/api', methods=['GET'])
 def index():
     return render_template("mittaukset.html", taulukko=lampotilat, paivat=paivat)
+
+@app.route('/lisaa', methods=['POST'])
+def lisaa():
+    uusimittaus = request.get_json(force=True)
+    lampotilat.append(uusimittaus)
+    return(json.dumps(uusimittaus))
 
 if __name__ == "__main__":
     app.run(debug=True)
