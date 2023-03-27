555579a6a7da43726ad290560a1ac1c11024c123
Jani M <jani.maki@seamk.fi>
2023-03-27 12:05:32 +0300

    lisätty post lähetys

---
+++ laheta_pyynto.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+mittaus = {"x": 6,"y": 12}
+
+while(True):
+    mittaus["x"] = int(input("Anna viikonpäivä: "))
+    mittaus["y"] = int(input("Anna lämpötila: "))
+
+    viesti = json.dumps(mittaus)
+    # /api on mistä voi tarkastella. /lisaa jos halutaan survoa tietoa johonkin
+    vastaus = requests.post('http://127.0.0.1:5000/lisaa', data=viesti)
+
